582e234839cb2418671237d5ef63572977a97384
Sudipta638 <sudiptadas2303@gmail.com>
2023-12-23 23:03:40 +0530

    Add project files.

---
+++ Weather_App/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Xml.Linq;
+using System;
+using Weather_App.Models;
+
+namespace Weather_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<WeatherDatacs> weatherlist = new List<WeatherDatacs>()
+            {
+               new WeatherDatacs(){
+                   CityUniqueCode = "LDN",
+                   CityName = "London",
+                   DateAndTime = DateTime.Parse("2030-01-01 8:00"),
+                   TemperatureFahrenheit = 33
+                   },
+               new WeatherDatacs(){
+                   CityUniqueCode = "NYC",
+                   CityName = "New York",
+                   DateAndTime = DateTime.Parse("2030-01-01 3:00"),
+                   TemperatureFahrenheit = 60
+                   },
+               new WeatherDatacs(){
+                   CityUniqueCode = "PAR", 
+                   CityName = "Paris",
+                   DateAndTime = DateTime.Parse("2030-01-01 9:00"), 
+                   TemperatureFahrenheit = 82
+
+                  }
+            };
+
+            return View(weatherlist);
+        }
+        [Route("/weather/{code}")]
+        public IActionResult Cityweather(string ? code)
+        {
+            if (code == null)
+                return Content("Person name can't be null");
+            List<WeatherDatacs> weatherlist = new List<WeatherDatacs>()
+            {
+               new WeatherDatacs(){
+                   CityUniqueCode = "LDN",
+                   CityName = "London",
+                   DateAndTime = DateTime.Parse("2030-01-01 8:00"),
+                   TemperatureFahrenheit = 33
+                   },
+               new WeatherDatacs(){
+                   CityUniqueCode = "NYC",
+                   CityName = "New York",
+                   DateAndTime = DateTime.Parse("2030-01-01 3:00"),
+                   TemperatureFahrenheit = 60
+                   },
+               new WeatherDatacs(){
+                   CityUniqueCode = "PAR",
+                   CityName = "Paris",
+                   DateAndTime = DateTime.Parse("2030-01-01 9:00"),
+                   TemperatureFahrenheit = 82
+
+                  }
+            };
+
+            WeatherDatacs? matchingcity = weatherlist.Where(temp => temp.CityUniqueCode == code).FirstOrDefault();
+            return View(matchingcity);
+        }
+
+    }
+}
---
+++ Weather_App/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllers();
+//sapp.MapGet("/", () => "Hello World!");
+
+app.Run();
